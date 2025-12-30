c02dd558a6529475330c349c7750b1341acbd7a7
Aratare-official <BlackRedBelt123@gmail.com>
2025-12-30 14:03:34 +0100

    Update svchost.bat

--- svchost.bat
+++ svchost.bat
@@ -1,2 +1,21 @@
-echo off
-echo hi
+@echo off
+title Popup Message
+color 0A
+
+echo Creating popup message...
+
+:: Create VBS script to show popup
+echo Set objShell = CreateObject("WScript.Shell") > "%TEMP%\popup.vbs"
+echo objShell.Popup "Hello! This is a popup message.", 5, "System Message", 64 >> "%TEMP%\popup.vbs"
+
+:: Run the popup
+cscript //nologo "%TEMP%\popup.vbs"
+
+:: Cleanup
+del "%TEMP%\popup.vbs" 2>nul
+
+echo Popup displayed!
+echo.
+echo Press any key to exit...
+pause >nul
+exit
